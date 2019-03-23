4348202fcadf0f7f123c9f357dbb2bec367d6cd9
Christopher Gauffin <christopher.gauffin@gmail.com>
2019-03-23 16:48:55 +0100

    no buzzerino

--- Datorteknik/project/buzzer.c
+++ Datorteknik/project/buzzer.c
@@ -23,50 +23,6 @@ void playTone(int freq, int duration) {
     pauseBuzzer();
 }
 
-char notes[] = "ccggaagffeeddc "; // a space represents a rest
-/* char notes[] = "deeeddddffffeeedeeeddddffffeeedeee"; // a space represents a rest */
-int beats[] = { 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 4 };
-int tempo = 300;
-int length = sizeof(notes) / sizeof(notes[0]); // the number of notes
-
-void playTon(int tone, int duration) {
-    for (long i = 0; i < duration * 1000L; i += tone * 2) {
-        playBuzzer();
-        delaymicros(tone);
-        pauseBuzzer();
-        delaymicros(tone);
-    }
-}
-
-void playNote(char note, int duration) {
-    char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
-    int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
-
-    // play the tone corresponding to the note name
-    for (int i = 0; i < 8; i++) {
-        if (names[i] == note) {
-            playTone(tones[i], duration);
-        }
-    }
-}
-
-void loop() {
-    for (int i = 0; i < length; i++) {
-        if (notes[i] == ' ') {
-            delay(beats[i] * tempo); // rest
-        } else {
-            playNote(notes[i], beats[i] * tempo);
-        }
-
-        // pause between notes
-        delay(tempo / 2); 
-    }
-}
 void initBuzzer() {
-    resetTimer(3);
     OC4CON = 0xE;                                       // Set Timer3 as source | enable pwm mode without fault protection
-    startTimer(3);
-    /* while(1) { */
-    /*     loop(); */
-    /* } */
 }
