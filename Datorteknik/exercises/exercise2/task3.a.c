603fe09e96c46da18628d996b97b2f5e02c49e78
Christopher Gauffin <christopher.gauffin@gmail.com>
2019-04-11 16:22:52 +0200

    moved files

--- Datorteknik/exercises/exercise2/task3.a.c
+++
@@ -1,5 +0,0 @@
-int main() {
-    volatile int* trise = 0xBF886100;
-    *trise = *trise & ~(7 << 1);   // 0xFFF...F1
-
-}
--- Datorteknik/exercises/exercise2/task3.b.c
+++
@@ -1,6 +0,0 @@
-#include <pic32mc.h>
-
-int main() {
-    TRISE = TRISE & ~(7 << 1);
-    TRISECLR = (7 << 1);        // Same as above
-}
